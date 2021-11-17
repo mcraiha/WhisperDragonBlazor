a1f60dfdac6e9f97d77b65f851ecdaf75f95f430
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-11-17 18:32:51 +0200

    Add notes for testing
    Easier to develop

---
+++ src/Data/NoteSimplified.cs
@@ -0,0 +1,18 @@
+
+public sealed class NoteSimplified
+{
+    // Non visible
+    public int zeroBasedIndexNumber { get; set; }
+
+    // Visible elements
+
+    public bool IsSecure { get; set; }
+
+    public string Title { get; set; }
+
+    public string Text { get; set; }
+
+    public string CreationTime { get; set; }
+
+    public string ModificationTime { get; set; }
+}
