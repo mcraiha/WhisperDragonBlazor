735fa53cd91dcf5b4f093320cdc75ed53a6ae3a1
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-11-18 18:33:19 +0200

    Add ShowContacts
    One more

---
+++ src/Data/ContactSimplified.cs
@@ -0,0 +1,21 @@
+public sealed class ContactSimplified
+{
+    // Non visible
+    public int zeroBasedIndexNumber { get; set; }
+
+    // Visible elements
+
+    public bool IsSecure { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Emails { get; set; }
+
+    public string PhoneNumbers { get; set; }
+
+    public string CreationTime { get; set; }
+
+    public string ModificationTime { get; set; }
+}
