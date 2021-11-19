46a7b3a3cf3730e19ac040c35aad40fc1e642741
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-11-19 17:59:34 +0200

    Add Show payment cards
    All simplified shows are now in

---
+++ src/Data/PaymentCardSimplified.cs
@@ -0,0 +1,19 @@
+public sealed class PaymentCardSimplified
+{
+    // Non visible
+    public int zeroBasedIndexNumber { get; set; }
+
+    // Visible elements
+
+    public bool IsSecure { get; set; }
+
+    public string Title { get; set; }
+
+    public string NameOnTheCard { get; set; }
+
+    public string CardType { get; set; }
+
+    public string Number { get; set; }
+
+    public string SecurityCode { get; set; }
+}
