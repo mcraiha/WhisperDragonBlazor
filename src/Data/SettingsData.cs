01750c29593d5ba0bf6d415e6e82ebc6738d9d8a
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-12-07 17:25:19 +0200

    Add some tools for formatting text
    Not needed ATM but will be useful later on

---
+++ src/Data/SettingsData.cs
@@ -0,0 +1,45 @@
+
+public enum ShowMode
+{
+	ShowFull = 0,
+	ShowFirstFour,
+	ShowFirst,
+	HiddenCorrectLength,
+	HiddenConstantLenght,
+	HiddenRandomLength
+}
+
+/// <summary>
+/// All settings for WhisperDragonBlazor
+/// </summary>
+public sealed class SettingsData
+{
+	// Logins
+	public ShowMode LoginTitleShowMode { get; set; } = ShowMode.ShowFull;
+
+	public ShowMode LoginUrlShowMode { get; set; } = ShowMode.ShowFull;
+
+	public ShowMode LoginEmailShowMode { get; set; } = ShowMode.ShowFull;
+
+	public ShowMode LoginUsernameShowMode { get; set; } = ShowMode.ShowFull;
+
+	public ShowMode LoginPasswordShowMode { get; set; } = ShowMode.HiddenConstantLenght;
+
+	public ShowMode LoginCategoryShowMode { get; set; } = ShowMode.ShowFull;
+
+
+	// Notes
+	public ShowMode NoteTitleShowMode { get; set; } = ShowMode.ShowFull;
+	public ShowMode NoteTextShowMode { get; set; } = ShowMode.HiddenConstantLenght;
+
+
+	// Files
+	public ShowMode FileFilenameShowMode { get; set; } = ShowMode.ShowFull;
+	public ShowMode FileFileSizeShowMode { get; set; } = ShowMode.ShowFull;
+	public ShowMode FileFileTypeShowMode { get; set; } = ShowMode.ShowFull;
+	
+	public SettingsData()
+	{
+
+	}
+}
