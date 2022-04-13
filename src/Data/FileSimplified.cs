2bd864afa65413c52e33dfdd095a58eeb51d255e
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-04-13 20:23:06 +0300

    Add CreateFileSimplifieds method
    Will be used soon

--- src/Data/FileSimplified.cs
+++ src/Data/FileSimplified.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CSCommonSecrets;
+
 public sealed class FileSimplified
 {
     // Non visible
@@ -16,4 +20,48 @@ public sealed class FileSimplified
     public string CreationTime { get; set; }
 
     public string ModificationTime { get; set; }
+
+    private static readonly string dateTimeOffsetToStringConvert = "u";
+
+    public static async Task<FileSimplified[]> CreateFileSimplifieds(List<FileEntry> files, List<FileEntrySecret> fileSecret, ISecurityAsyncFunctions securityFunctions, IReadDerivedPassword derivedPasswords)
+    {
+        List<FileSimplified> returnValues = new List<FileSimplified>();
+
+        for (int i = 0; i < files.Count; i++)
+        {
+            FileSimplified fileeSimplified = new FileSimplified()
+            {
+                zeroBasedIndexNumber = i,
+                IsSecure = false,
+                Filename = files[i].GetFilename(),
+                Filesize = files[i].GetFileContentLengthInBytes().ToString(),
+                CreationTime = files[i].GetCreationTime().ToString(dateTimeOffsetToStringConvert),
+                ModificationTime = files[i].GetModificationTime().ToString(dateTimeOffsetToStringConvert),
+            };
+
+            returnValues.Add(fileeSimplified);
+        }
+
+        for (int i = 0; i < fileSecret.Count; i++)
+        {
+            string keyIdentifier = fileSecret[i].GetKeyIdentifier();
+            if (derivedPasswords.DoesDerivedPasswordExist(keyIdentifier))
+            {
+                byte[] derivedPassword = derivedPasswords.GetDerivedPassword(keyIdentifier);
+                FileSimplified fileeSimplified = new FileSimplified()
+                {
+                    zeroBasedIndexNumber = i,
+                    IsSecure = true,
+                    Filename = await fileSecret[i].GetFilenameAsync(derivedPassword, securityFunctions),
+                    Filesize = (await fileSecret[i].GetFileContentLengthInBytesAsync(derivedPassword, securityFunctions)).ToString(),
+                    CreationTime = (await fileSecret[i].GetCreationTimeAsync(derivedPassword, securityFunctions)).ToString(dateTimeOffsetToStringConvert),
+                    ModificationTime = (await fileSecret[i].GetModificationTimeAsync(derivedPassword, securityFunctions)).ToString(dateTimeOffsetToStringConvert),
+                };
+
+                returnValues.Add(fileeSimplified);
+            }
+        }
+
+        return returnValues.ToArray();
+    }
 }
