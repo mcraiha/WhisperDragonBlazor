38d49fac9ec014dc07d565e4a548246b775ed3db
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-04-17 21:24:11 +0300

    Add CreatePaymentCardSimplifieds method
    Needed later on

--- src/Data/PaymentCardSimplified.cs
+++ src/Data/PaymentCardSimplified.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CSCommonSecrets;
+
 public sealed class PaymentCardSimplified
 {
     // Non visible
@@ -16,4 +20,58 @@ public sealed class PaymentCardSimplified
     public string Number { get; set; }
 
     public string SecurityCode { get; set; }
+
+    public string CreationTime { get; set; }
+
+    public string ModificationTime { get; set; }
+
+    private static readonly string dateTimeOffsetToStringConvert = "u";
+
+    public static async Task<PaymentCardSimplified[]> CreatePaymentCardSimplifieds(List<PaymentCard> paymentCards, List<PaymentCardSecret> paymentCardSecrets, ISecurityAsyncFunctions securityFunctions, IReadDerivedPassword derivedPasswords)
+    {
+        List<PaymentCardSimplified> returnValues = new List<PaymentCardSimplified>();
+
+        for (int i = 0; i < paymentCards.Count; i++)
+        {
+            PaymentCardSimplified paymentCardSimplified = new PaymentCardSimplified()
+            {
+                zeroBasedIndexNumber = i,
+                IsSecure = false,
+                Title = paymentCards[i].GetTitle(),
+                NameOnTheCard = paymentCards[i].GetNameOnCard(),
+                CardType = paymentCards[i].GetCardType(),
+                Number = paymentCards[i].GetNumber(),
+                SecurityCode = paymentCards[i].GetSecurityCode(),
+                CreationTime = paymentCards[i].GetCreationTime().ToString(dateTimeOffsetToStringConvert),
+                ModificationTime = paymentCards[i].GetModificationTime().ToString(dateTimeOffsetToStringConvert),
+            };
+
+            returnValues.Add(paymentCardSimplified);
+        }
+
+        for (int i = 0; i < paymentCardSecrets.Count; i++)
+        {
+            string keyIdentifier = paymentCardSecrets[i].GetKeyIdentifier();
+            if (derivedPasswords.DoesDerivedPasswordExist(keyIdentifier))
+            {
+                byte[] derivedPassword = derivedPasswords.GetDerivedPassword(keyIdentifier);
+                PaymentCardSimplified paymentCardSimplified = new PaymentCardSimplified()
+                {
+                    zeroBasedIndexNumber = i,
+                    IsSecure = true,
+                    Title = await paymentCardSecrets[i].GetTitleAsync(derivedPassword, securityFunctions),
+                    NameOnTheCard = await paymentCardSecrets[i].GetNameOnCardAsync(derivedPassword, securityFunctions),
+                    CardType = await paymentCardSecrets[i].GetCardTypeAsync(derivedPassword, securityFunctions),
+                    Number = await paymentCardSecrets[i].GetNumberAsync(derivedPassword, securityFunctions),
+                    SecurityCode = await paymentCardSecrets[i].GetSecurityCodeAsync(derivedPassword, securityFunctions),
+                    CreationTime = (await paymentCardSecrets[i].GetCreationTimeAsync(derivedPassword, securityFunctions)).ToString(dateTimeOffsetToStringConvert),
+                    ModificationTime = (await paymentCardSecrets[i].GetModificationTimeAsync(derivedPassword, securityFunctions)).ToString(dateTimeOffsetToStringConvert),
+                };
+
+                returnValues.Add(paymentCardSimplified);
+            }
+        }
+
+        return returnValues.ToArray();
+    }
 }
